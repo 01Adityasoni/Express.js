9b1567ac52ccdf233da4a3dfb98470f14e38561f
Aditya <adityasara2622@gmail.com>
2026-02-11 22:37:25 +0530

    backend start

---
+++ index.js
@@ -0,0 +1,30 @@
+ // import express from 'express';
+ const express = require('express');
+
+  // create an express app
+ const app = express();
+
+ // define a port to listen on
+    const port = 3000;
+
+    // add middleware to parse JSON request bodies
+    app.use(express.json());
+
+
+    // get route or get request
+    app.get('/', (req, res) => {
+      res.send('Hello, World!');
+    });
+
+    // post route or post request 
+    app.post('/car', (req,res) => {
+      res.send("recieve a post request to /car");
+     });
+
+
+
+
+    // start the server
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
